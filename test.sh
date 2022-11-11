c70361efada0d3ddb3d5e2cc82458785fd66bd9f
sheetal104 <sheetals410@gmail.com>
2022-11-11 12:46:52 +0530

    compile and run the binary

--- test.sh
+++ test.sh
@@ -8,11 +8,13 @@ do
 		cd src
 		make clobber
 		make lmbench
+		# modify executable name
 		gcc -O -DRUSAGE -DHAVE_uint=1 -DHAVE_int64_t=1 -DHAVE_pmap_clnt_h -DHAVE_socklen_t -DHAVE_DRAND48 -DHAVE_SCHED_SETAFFINITY=1   -o ../bin/x86_64-linux-gnu/par_rw_test par_rw_test.c ../bin/x86_64-linux-gnu/lmbench.a -lm -lpthread
 
 		# run
 		cd ..
 		cd bin/x86_64-linux-gnu
+		# modify arguments
 		./par_rw_test 4 63562 ../../../freqs.json
 		echo "DONE"
 		exit
