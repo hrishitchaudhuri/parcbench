a5ae6ef3eaf40dc38d2a1aa0610b3958b56e5beb
harsh188 <hiharshith18@gmail.com>
2022-12-12 22:13:56 +0530

    omg multiple threads works wtf jesus christ thank god imma go sleep

--- lmbench-3.0-a9/src/par_rw.c
+++ lmbench-3.0-a9/src/par_rw.c
@@ -2,7 +2,9 @@
 #include <pthread.h>
 #include <time.h>
 #include "lib_timing.c"
+#include <sys/syscall.h>
 
+#define gettidv1() syscall(__NR_gettid) // new form
 #define MINSZ (sizeof(TYPE) * 128)
 #define CHK(x)          \
 	if ((int)(x) == -1) \
@@ -18,6 +20,7 @@ size_t count;	 /* bytes to move (can't be modified) */
 struct arg_struct {
 	int iterations;
 	void *cookie;
+	int p_id;
 };
 
 typedef struct _state
@@ -94,54 +97,51 @@ void init_open(iter_t iterations, void *cookie)
 void *time_with_open(void *arguments)
 {
 	struct arg_struct *args = arguments;
-
 	state_t *state = (state_t *)args->cookie;
 	char *filename = state->filename;
 	int itr = args->iterations; 
 	int fd;
-
-	
+	printf("\n I am doing time with open, p_id: %ld", (long int)gettidv1());
 
 	while (itr-- > 0)
 	{
-		printf("\n I am doing time with open");
-		printf("\n Iteration: %d \n",itr);
 		if ((int)(fd = open(state->filename, O_RDONLY)) == -1) { 
 			printf("%s\n", state->filename);
 			perror("fd = open(state->filename, O_RDONLY)"); 
 			exit(1); 
     	}
 
-    	printf("[INIT] File descriptor: %d\n", fd);
+    	printf("\n Iteration, [INIT] File descriptor: %d %d\n", itr, fd);
 		state->fd = fd;
 
 		doit(state->fd);
 		close(state->fd);
-		printf("I am done with time with open");
 	}
-	pthread_exit(NULL);
+	printf("I am done with time with open, p_id: %ld", (long int)gettidv1());
+	// pthread_exit(NULL);
 	return NULL;
 }
 
-void time_io_only(void *arguments)
+void *time_io_only(void *arguments)
 {
 	struct arg_struct *args = arguments;
 
 	state_t *state = (state_t *)args->cookie;
 	char *filename = state->filename;
 	int itr = args->iterations; 
-
-
 	int fd = state->fd;
+	printf("\n I am doing time io, p_id: %ld", (long int)gettidv1());
 
 	while (itr-- > 0)
 	{
-		printf("\n I am doing time io");
 		lseek(state->fd, 0, SEEK_SET); // Moves file pointer to beginning of file descriptor
 		doit(state->fd);
-		printf("I am done with time io");
 
 	}
+	printf("I am done with time io, p_id: %ld", (long int)gettidv1());
+	// pthread_exit(NULL);
+	return NULL;
+
 }
 
 void cleanup(iter_t iterations, void *cookie)
@@ -158,42 +158,44 @@ void cleanup(iter_t iterations, void *cookie)
 }
 
 void parallel_open(iter_t iterations, void *cookie){
-    printf("yo bro parallel open");
+    printf("----------------------------------------------------");
     pthread_t thread[2];
-	struct arg_struct args;
-	printf("yo before args: %lu \n",iterations);
-	args.iterations = iterations;
-	args.cookie = cookie;
 
     for (int i = 0; i < 2; i ++)
     {
-    	printf("\nPOpen - Creating thread %d",i);
+		struct arg_struct args;
+		args.p_id = i;
+		args.iterations = iterations;
+		args.cookie = cookie;
         pthread_create(&thread[i], NULL, time_with_open, (void *)&args);
+    	printf("\nPOpen - Created thread %ld",(long int)gettidv1());
     }
     for (int i = 0; i < 2; i ++)
     {
-    	printf("\nPOpen - Joining thread %d",i);
-        pthread_join(&thread[i], NULL);
+    	printf("\nPOpen - Joining thread %ld",(long int)gettidv1());
+        pthread_join(thread[i], NULL);
     }
+    printf("----------------------------------------------------");
 
 }
 
 void parallel_seek(iter_t iterations, void *cookie) {
     printf("yo bro parallel seek");
 	pthread_t thread[2];
-	struct arg_struct args;
-	args.iterations = iterations;
-	args.cookie = cookie;
 
     for (int i = 0; i < 2; i ++)
     {
-    	printf("\nPSeek - Creating thread %d",i);
+		struct arg_struct args;
+		args.iterations = iterations;
+		args.cookie = cookie;
+		args.p_id = i;
         pthread_create(&thread[i], NULL, time_io_only, (void *)&args);
+		printf("\nPSeek - Created thread %ld",(long int)gettidv1());
     }
     for (int i = 0; i < 2; i ++)
     {
-    	printf("\nPSeek - Joining thread %d",i);
-        pthread_join(&thread[i], NULL);
+    	printf("\nPSeek - Joining thread %ld",(long int)gettidv1());
+        pthread_join(thread[i], NULL);
     }
 }
 
